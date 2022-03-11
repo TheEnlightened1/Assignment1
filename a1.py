c37096c4239ef3ed8eafb9374acc7fcbcce9e99a
TheEnlightened1 <93515955+TheEnlightened1@users.noreply.github.com>
2022-03-11 19:15:31 +1000

    First initial commit of a1 and drafts. Both untouched

---
+++ a1.py
@@ -0,0 +1,41 @@
+"""
+Assignment 1
+Semester 1, 2022
+ENGG1001
+"""
+
+# Replace these <strings> with your name, student number and email address.
+__author__ = "<Your Name>, <Your Student Number>"
+__email__ = "<Your Student Email>"
+
+HELP = """
+    'i' - prompt for the input parameters
+    'd' - determine distances to lift-off for the current input parameters
+    'p <increments> <step>' - print out a table of distances to lift-off for different drag coefficients
+    'h' - help message
+    'q' - quit
+"""
+
+def prompt_for_inputs() -> tuple[tuple[float, ...], float]:
+    """
+    """
+    pass
+
+def compute_trajectory(values: tuple[float, ...], 
+        drag_coeff: float) -> tuple[tuple[float, ...], tuple[float, ...]]:
+    """
+    """
+    pass
+
+
+def print_table(values: tuple[float, ...], drag_coeff: float, increments: int, step: float):
+    """
+    """
+    pass
+
+def main():
+    """Entry point to interaction"""
+    print("Implement your solution and run this file")
+
+if __name__ == "__main__":
+    main()
