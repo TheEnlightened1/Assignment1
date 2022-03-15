4afcbccff4a5d95df8be3d2f63e3b98d1354147b
TheEnlightened1 <93515955+TheEnlightened1@users.noreply.github.com>
2022-03-15 18:32:05 +1000

    Upto Task 3, know how to do it. Working on functionality first.

--- DRAFTS.py
+++ DRAFTS.py
@@ -41,7 +41,7 @@ def compute_trajectory(values, drag_coeff):
     # x0 = 0
     # t_i = 0.1
     # drag_coeff = 0.015
-    # # #breakpoint()
+    # breakpoint()
     a_i = 1/m * (F_thrust - 0.5 * p * v0**2 * A_ref * drag_coeff)
 
     velocities = []
@@ -54,22 +54,57 @@ def compute_trajectory(values, drag_coeff):
 
     # velocity_full = ()
 
-    breakpoint()
+    #breakpoint()
     while velocity < v_lift:
 
         time_delta += t_i
         velocity = v0 + (a_i*time_delta)
-        format.round(velocity, 3)
+        round(velocity, 3)
         #breakpoint()
         velocities.append(velocity)
         position = x0 + velocity * time_delta + 0.5 * a_i * (time_delta**2)
-        position += position
+        #position += position
         round(position, 3)
         positions.append(position)
 
 
     return tuple(velocities), tuple(positions)
- ###Printing values for velocity and position fine but I need to return them as a tuple of the results
+
+#def print_table(values, drag_coeff, increments, step):
+def print_table(values, drag_coeff):
+    m = values[0]
+    F_thrust = values[1]
+    A_ref = values[2]
+    p = values[3]
+    v0 = values[4]
+    v_lift = values[5]
+    x0 = values[6]
+    t_i = values[7]
+
+    time_delta = 0
+
+    a_i = 1/m * (F_thrust - 0.5 * p * v0**2 * A_ref * drag_coeff)
+
+    velocity = v0 + (a_i*time_delta)
+
+    while velocity < v_lift:
+        time_delta += t_i
+        velocity = v0 + (a_i*time_delta)
+        #breakpoint()
+        position = x0 + velocity * time_delta + 0.5 * a_i * (time_delta**2)
+        #position += position
+        print(position)
+
+##Have to find a way to get the last position from a set of drag_coeff's. Funnily enough this is where we wanted the behaviour of 
+##only getting the highest most variable in the last function. Some effective python chap one and two tips could come in really handy here like
+## enumerate, zip, catch all unpacking and multiple assignment unpacking
+        
+
+
+
+    
+
+
 
 ######################################
 ######################################
@@ -78,7 +113,9 @@ values, drag_coeff = prompt_for_inputs()
 
 #velocities, positions = compute_trajectory()
 
-print(compute_trajectory(values, drag_coeff))
+#print(compute_trajectory(values, drag_coeff))
+
+print(print_table(values, drag_coeff))
 
 
 
