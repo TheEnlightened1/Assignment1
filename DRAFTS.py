97bfc4ee640645f76e9fa8455cba502ab3fc314e
TheEnlightened1 <93515955+TheEnlightened1@users.noreply.github.com>
2022-03-16 16:47:03 +1000

    Working task 1,2,3 functionally. Need to rework task 3 for print table

--- DRAFTS.py
+++ DRAFTS.py
@@ -70,52 +70,81 @@ def compute_trajectory(values, drag_coeff):
 
     return tuple(velocities), tuple(positions)
 
-#def print_table(values, drag_coeff, increments, step):
-def print_table(values, drag_coeff):
-    m = values[0]
-    F_thrust = values[1]
-    A_ref = values[2]
-    p = values[3]
-    v0 = values[4]
-    v_lift = values[5]
-    x0 = values[6]
-    t_i = values[7]
+def print_table(increments, step):
+#def print_table(values, drag_coeff):
+    # m = values[0]
+    # F_thrust = values[1]
+    # A_ref = values[2]
+    # p = values[3]
+    # v0 = values[4]
+    # v_lift = values[5]
+    # x0 = values[6]
+    # t_i = values[7]
+    m = 50000
+    F_thrust = 600000
+    A_ref = 800
+    p = 1
+    v0 = 0
+    v_lift = 70
+    x0 = 0
+    t_i = 0.1
+    drag_coeff = 0.015
 
     time_delta = 0
 
-    a_i = 1/m * (F_thrust - 0.5 * p * v0**2 * A_ref * drag_coeff)
+    position = 0
 
-    velocity = v0 + (a_i*time_delta)
+    new_drag = 0
 
-    while velocity < v_lift:
-        time_delta += t_i
-        velocity = v0 + (a_i*time_delta)
-        #breakpoint()
-        position = x0 + velocity * time_delta + 0.5 * a_i * (time_delta**2)
-        #position += position
+    velocity = 0
+
+    #velocity = True
+
+    #velocity = v0 + (a_i*time_delta)
+    #breakpoint()
+    for i in range(0, increments):
         print(position)
+        drag_coeff += step
+        velocity = 0
+        while velocity < v_lift:
+                time_delta += t_i
+                a_i = 1/m * (F_thrust - 0.5 * p * v0**2 * A_ref * drag_coeff)
+                velocity = v0 + (a_i*time_delta)
+                position = x0 + velocity * time_delta + 0.5 * a_i * (time_delta**2)
 
-##Have to find a way to get the last position from a set of drag_coeff's. Funnily enough this is where we wanted the behaviour of 
+
+
+
+
+
+
+
+
+
+
+##Have to find a way to get the last position from a set of drag_coeff's. Funnily enough this is where we wanted the behaviour of
 ##only getting the highest most variable in the last function. Some effective python chap one and two tips could come in really handy here like
 ## enumerate, zip, catch all unpacking and multiple assignment unpacking
-        
 
 
 
-    
+
+
 
 
 
 ######################################
 ######################################
 # print(prompt_for_inputs())
-values, drag_coeff = prompt_for_inputs()
+# values, drag_coeff = prompt_for_inputs()
 
 #velocities, positions = compute_trajectory()
 
 #print(compute_trajectory(values, drag_coeff))
 
-print(print_table(values, drag_coeff))
+# print(print_table(values, drag_coeff, 10, 0.03))
+
+print(print_table(10, 0.03))
 
 
 
--- a1.py
+++ a1.py
@@ -17,15 +17,50 @@ HELP = """
 """
 
 def prompt_for_inputs() -> tuple[tuple[float, ...], float]:
-    """
-    """
-    pass
+    values = float(input("Input mass of plane: ")), float(input("Input engine force (in N): ")), float(input("Input reference area (in mˆ2): ")), \
+    float(input("Input air density (in kg/mˆ3): ")), float(input("Input initial velocity at start of runway (in m/s): ")), \
+    float(input("Input lift-off velocity (in m/s): ")), float(input("Input position of the start of the runway (in m): ")), \
+    float(input("Input time increment (in secs):  ")),
 
-def compute_trajectory(values: tuple[float, ...], 
+    drag_coeff = float(input("Input drag coefficient:   "))
+
+    return values, drag_coeff
+
+def compute_trajectory(values: tuple[float, ...],
         drag_coeff: float) -> tuple[tuple[float, ...], tuple[float, ...]]:
-    """
-    """
-    pass
+    m = values[0]
+    F_thrust = values[1]
+    A_ref = values[2]
+    p = values[3]
+    v0 = values[4]
+    v_lift = values[5]
+    x0 = values[6]
+    t_i = values[7]
+
+    a_i = 1/m * (F_thrust - 0.5 * p * v0**2 * A_ref * drag_coeff)
+
+    velocities = []
+
+    positions = []
+
+    time_delta = 0
+
+    velocity = v0 + (a_i*time_delta)
+
+    while velocity < v_lift:
+
+        time_delta += t_i
+        velocity = v0 + (a_i*time_delta)
+        round(velocity, 3)#Not working?
+            #breakpoint()
+        velocities.append(velocity)
+        position = x0 + velocity * time_delta + 0.5 * a_i * (time_delta**2)
+        position += position
+        round(position, 3)#Not working?
+        positions.append(position)
+
+
+    return tuple(velocities), tuple(positions)
 
 
 def print_table(values: tuple[float, ...], drag_coeff: float, increments: int, step: float):
